c9c89a21df98ffba967d3c1960500f80e410352b
kmoonwright <kmoonwright@outlook.com>
2022-12-19 18:55:48 -0800

    Instrument OpenTelemetry and add CustomActivitySource

--- Billing/OrderPlacedHandler.cs
+++ Billing/OrderPlacedHandler.cs
@@ -1,7 +1,12 @@
-﻿using System.Threading.Tasks;
+﻿using System.Diagnostics;
+using System.Threading.Tasks;
 using Messages;
 using NServiceBus;
 using NServiceBus.Logging;
+using OpenTelemetry;
+using OpenTelemetry.Exporter;
+using OpenTelemetry.Resources;
+using OpenTelemetry.Trace;
 
 namespace Billing
 {
@@ -10,11 +15,19 @@ namespace Billing
         IHandleMessages<OrderPlaced>
     {
         static readonly ILog log = LogManager.GetLogger<OrderPlacedHandler>();
+        static class CustomActivitySources
+        {
+            public const string Name = "Example.Billing";
+            public static ActivitySource Main = new ActivitySource(Name);
+        }
 
         public Task Handle(OrderPlaced message, IMessageHandlerContext context)
         {
-            log.Info($"Billing has received OrderPlaced, OrderId = {message.OrderId}");
-            return Task.CompletedTask;
+            using (var activity = CustomActivitySources.Main.StartActivity("OrderPlaced"))
+            {
+                log.Info($"Billing has received OrderPlaced, OrderId = {message.OrderId}");
+                return Task.CompletedTask;
+            }
         }
     }
 }
--- Billing/Program.cs
+++ Billing/Program.cs
@@ -1,7 +1,12 @@
 ﻿using System;
+using System.Diagnostics;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Hosting;
 using NServiceBus;
+using OpenTelemetry;
+using OpenTelemetry.Exporter;
+using OpenTelemetry.Resources;
+using OpenTelemetry.Trace;
 
 namespace Billing
 {    
@@ -13,24 +18,55 @@ namespace Billing
             await CreateHostBuilder(args).RunConsoleAsync();
         }
 
+        // Custom ActivitySource to export via OpenTelemetry
+        static class CustomActivitySources
+        {
+            public const string Name = "Example.Billing";
+            public static ActivitySource Main = new ActivitySource(Name);
+        }
+
         public static IHostBuilder CreateHostBuilder(string[] args)
         {
             return Host.CreateDefaultBuilder(args)
-                       .UseNServiceBus(context =>
-                       {
-                           var endpointConfiguration = new EndpointConfiguration("Billing");
+                .UseNServiceBus(context =>
+                {
+                    var endpointConfiguration = new EndpointConfiguration("Billing");
 
-                           endpointConfiguration.UseTransport<LearningTransport>();
+                    endpointConfiguration.UseTransport<LearningTransport>();
 
-                           endpointConfiguration.SendFailedMessagesTo("error");
-                           endpointConfiguration.AuditProcessedMessagesTo("audit");
-                           endpointConfiguration.SendHeartbeatTo("Particular.ServiceControl");
+                    endpointConfiguration.SendFailedMessagesTo("error");
+                    endpointConfiguration.AuditProcessedMessagesTo("audit");
+                    endpointConfiguration.SendHeartbeatTo("Particular.ServiceControl");
 
-                           var metrics = endpointConfiguration.EnableMetrics();
-                           metrics.SendMetricDataToServiceControl("Particular.Monitoring", TimeSpan.FromMilliseconds(500));
+                    var metrics = endpointConfiguration.EnableMetrics();
+                    metrics.SendMetricDataToServiceControl("Particular.Monitoring", TimeSpan.FromMilliseconds(500));
+                    
+                    // Open Telemetry Configurations for Billing Service
+                    endpointConfiguration.EnableOpenTelemetry();
+                    var serviceName = "Billing";
+                    var serviceVersion = "1.0.0";
+                    var APIKey = System.Environment.GetEnvironmentVariable("HONEYCOMB_API_KEY");
+                    var tracerProvider = Sdk.CreateTracerProviderBuilder()
+                        .AddOtlpExporter(option =>
+                        {
+                            option.Endpoint = new Uri("https://api.honeycomb.io/v1/traces");
+                            option.Headers = $"x-honeycomb-team={APIKey}";
+                            option.Protocol = OtlpExportProtocol.HttpProtobuf;
+                        })
+                        .SetResourceBuilder(
+                            ResourceBuilder
+                                .CreateDefault()
+                                .AddService(
+                                    serviceName: serviceName,
+                                    serviceVersion: serviceVersion
+                                )
+                        )
+                        .AddSource("NServiceBus.core")
+                        .AddSource(CustomActivitySources.Name)
+                        .Build();
 
-                           return endpointConfiguration;
-                       });
+                    return endpointConfiguration;
+                });
         }
     }
 }
