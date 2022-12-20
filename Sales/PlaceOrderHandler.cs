5490fdd6d859e052abad4fc0279d9313150e3b44
kmoonwright <kmoonwright@outlook.com>
2022-12-19 18:36:41 -0800

    Add OpenTelemetry to Sales Service

--- Sales/PlaceOrderHandler.cs
+++ Sales/PlaceOrderHandler.cs
@@ -1,8 +1,13 @@
 ﻿using System;
+using System.Diagnostics;
 using System.Threading.Tasks;
 using Messages;
 using NServiceBus;
 using NServiceBus.Logging;
+using OpenTelemetry;
+using OpenTelemetry.Exporter;
+using OpenTelemetry.Resources;
+using OpenTelemetry.Trace;
 
 namespace Sales
 {    
@@ -11,34 +16,44 @@ namespace Sales
     {
         static readonly ILog log = LogManager.GetLogger<PlaceOrderHandler>();
         static readonly Random random = new Random();
+        static class CustomActivitySources
+        {
+            public const string Name = "Example.SalesProcess";
+            public static ActivitySource Main = new ActivitySource(Name);
+        }
 
         public Task Handle(PlaceOrder message, IMessageHandlerContext context)
         {
-            log.Info($"Received PlaceOrder, OrderId = {message.OrderId}");
+            using (var activity = CustomActivitySources.Main.StartActivity("ProcessOrder")){
+                log.Info($"Received PlaceOrder, OrderId = {message.OrderId}");
 
-            // This is normally where some business logic would occur
+                // This is normally where some business logic would occur
 
-            #region ThrowTransientException
-            // Uncomment to test throwing transient exceptions
-            //if (random.Next(0, 5) == 0)
-            //{
-            //    throw new Exception("Oops");
-            //}
-            #endregion
+                #region ThrowTransientException
+                // Uncomment to test throwing transient exceptions
+                //if (random.Next(0, 5) == 0)
+                //{
+                //    throw new Exception("Oops");
+                //}
+                #endregion
 
-            #region ThrowFatalException
-            // Uncomment to test throwing fatal exceptions
-            //throw new Exception("BOOM");
-            #endregion
+                #region ThrowFatalException
+                // Uncomment to test throwing fatal exceptions
+                //throw new Exception("BOOM");
+                #endregion
 
-            var orderPlaced = new OrderPlaced
-            {
-                OrderId = message.OrderId
-            };
+                var orderPlaced = new OrderPlaced
+                {
+                    OrderId = message.OrderId
+                };
 
-            log.Info($"Publishing OrderPlaced, OrderId = {message.OrderId}");
+                // Add a custom attribute to OpenTelemetry Span
+                activity?.SetTag("orderId", message.OrderId);
 
-            return context.Publish(orderPlaced);
+                log.Info($"Publishing OrderPlaced, OrderId = {message.OrderId}");
+
+                return context.Publish(orderPlaced);
+            }
         }
     }
 }
--- Sales/Program.cs
+++ Sales/Program.cs
@@ -1,7 +1,12 @@
 ﻿using System;
+using System.Diagnostics;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Hosting;
 using NServiceBus;
+using OpenTelemetry;
+using OpenTelemetry.Exporter;
+using OpenTelemetry.Resources;
+using OpenTelemetry.Trace;
 
 namespace Sales
 {
@@ -13,34 +18,64 @@ namespace Sales
             await CreateHostBuilder(args).RunConsoleAsync();
         }
 
+        // Custom ActivitySource to export via OpenTelemetry
+        static class CustomActivitySources
+        {
+            public const string Name = "Example.SalesProcess";
+            public static ActivitySource Main = new ActivitySource(Name);
+        }
+
         public static IHostBuilder CreateHostBuilder(string[] args)
         {
             return Host.CreateDefaultBuilder(args)
-                       .UseNServiceBus(context =>
-                       {
-                           var endpointConfiguration = new EndpointConfiguration("Sales");
+                    .UseNServiceBus(context =>
+                    {
+                        var endpointConfiguration = new EndpointConfiguration("Sales");
 
-                           endpointConfiguration.UseTransport<LearningTransport>();
+                        endpointConfiguration.UseTransport<LearningTransport>();
 
-                           endpointConfiguration.SendFailedMessagesTo("error");
-                           endpointConfiguration.AuditProcessedMessagesTo("audit");
-                           endpointConfiguration.SendHeartbeatTo("Particular.ServiceControl");
+                        endpointConfiguration.SendFailedMessagesTo("error");
+                        endpointConfiguration.AuditProcessedMessagesTo("audit");
+                        endpointConfiguration.SendHeartbeatTo("Particular.ServiceControl");
 
-                           // So that when we test recoverability, we don't have to wait so long
-                           // for the failed message to be sent to the error queue
-                           var recoverablility = endpointConfiguration.Recoverability();
-                           recoverablility.Delayed(
-                               delayed =>
-                               {
-                                   delayed.TimeIncrease(TimeSpan.FromSeconds(2));
-                               }
-                           );
+                        // So that when we test recoverability, we don't have to wait so long
+                        // for the failed message to be sent to the error queue
+                        var recoverablility = endpointConfiguration.Recoverability();
+                        recoverablility.Delayed(
+                            delayed =>
+                            {
+                                delayed.TimeIncrease(TimeSpan.FromSeconds(2));
+                            }
+                        );
 
-                           var metrics = endpointConfiguration.EnableMetrics();
-                           metrics.SendMetricDataToServiceControl("Particular.Monitoring", TimeSpan.FromMilliseconds(500));
+                        var metrics = endpointConfiguration.EnableMetrics();
+                        metrics.SendMetricDataToServiceControl("Particular.Monitoring", TimeSpan.FromMilliseconds(500));
 
-                           return endpointConfiguration;
-                       });
+                        // Open Telemetry Configurations for Sales Service
+                        var serviceName = "Sales";
+                        var serviceVersion = "1.0.0";
+                        var APIKey = System.Environment.GetEnvironmentVariable("HONEYCOMB_API_KEY");
+                        var tracerProvider = Sdk.CreateTracerProviderBuilder()
+                            .AddOtlpExporter(option =>
+                            {
+                                option.Endpoint = new Uri("https://api.honeycomb.io/v1/traces");
+                                option.Headers = $"x-honeycomb-team={APIKey}";
+                                option.Protocol = OtlpExportProtocol.HttpProtobuf;
+                            })
+                            .SetResourceBuilder(
+                                ResourceBuilder
+                                    .CreateDefault()
+                                    .AddService(
+                                        serviceName: serviceName,
+                                        serviceVersion: serviceVersion
+                                    )
+                            )
+                            .AddSource("NServiceBus.core")
+                            .AddSource(CustomActivitySources.Name)
+                            .Build();
+
+                        return endpointConfiguration;
+                    });
         }
     }
 }
